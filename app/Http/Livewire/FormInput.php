a6221acf89cab3068d16cc8c8b757e8ab392a824
Dikshant Khatri <khatridikshant@outlook.com>
2024-11-12 14:37:24 +0545

    Animal Model Created

--- app/Http/Livewire/FormInput.php
+++ app/Http/Livewire/FormInput.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class FormInput extends Component
 {
+
+    public int $count;
+    
     public function render()
     {
         return view('livewire.form-input');
--- resources/views/livewire/form-input.blade.php
+++ resources/views/livewire/form-input.blade.php
@@ -1,3 +1,11 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <form>
+
+    <div class="mb-3">
+  <label for="animaldata" class="form-label">Animal Name</label>
+  <input type="text" class="form-control" name="animaldata" id="animaldata" placeholder="">
+</div>
+<textarea rows="3" class="form-control"> </textarea>
+<button type="button" class="btn btn-primary m-1">Add Data</button>
+    </form>
 </div>
